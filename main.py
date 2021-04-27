5c376567a6e3442a7096465cd96f5af31e94ffc3
yael-wolff <wolff.yael@gmail.com>
2021-04-27 13:00:17 +0300

    created class

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
 import sys
 from statistics import mean, median
+import pandas as pd
+
+
+class Data():
+    def __init__(self, path):
+        self.path = path
+        df = pd.read_csv(path)
+        self.df = df
+        self.data = df.to_dict(orient='list')
+
+    def load_data(self):
+        dictionary_data = self.data
+        file = open("data.py", "w")
+        file.write(str(dictionary_data))
+        file.close()
+
+    def get_all_districts(self):
+        columns = ['denominazione_region']
+        list_of_districts = pd.DataFrame(self.df, columns=columns)
+        list_of_districts = list_of_districts.values
+        return(list_of_districts)
+
+    def set_districts_data(self, districts):
+
+
 
 
 def main(argv):
-    pass
+#    path = sys.argv[1]
+    districts = ['Calabria', 'Umbria']
+    path = 'C:\\Users\Yael\Documents\intro_to_DS\hw2\dpc-covid19-ita-regioni.csv'
+    data1 = Data(path)
+    data1.load_data()
+    data1.get_all_districts()
+
+
+
+
+
 
 if __name__ == '__main__':
     main(sys.argv)
+
+#C://Users/Yael/Documents/intro_to_DS/hw2/main.py
